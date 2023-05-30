e538e43d723eac0ec26d532ceb6d5a67a0f05be6
furaha707 <zxcvyein950823@gmail.com>
2023-05-30 13:51:49 +0900

    Initial commit

---
+++ include/arr_data.php
@@ -0,0 +1,224 @@
+<?
+
+$site_title = "(주)명성아이지"; // 사이트명
+
+$P_lang = "ko"; // html language
+
+$P_company_name = "(주)명성아이지"; // 회사명
+
+$P_addr1 = "18525) 경기도 화성시 팔탄면 주석로 778번길 138-7"; // 주소1
+$P_addr2 = ""; // 주소2
+
+$P_tel1 = "031-353-0352";
+$P_tel2 = "";
+
+$P_fax1 = "031-370-6388";
+$P_fax2 = "";
+
+$P_email1 = "msig0352@naver.com";
+$P_email2 = "";
+
+$P_business_no = "143-81-08521"; // 사업자등록번호
+
+$P_sales_no = "화성팔탄-2013-0015"; // 통신판매업신고번호
+
+// 네비게이션 메뉴명
+$nav_1 = "브랜드";
+$nav_1_1 = "명성아이지는?";
+$nav_1_1_1 = "원스톱 솔루션";
+$nav_1_1_2 = "자체 기술력";
+$nav_1_1_3 = "품질 및 서비스";
+$nav_1_2 = "CI 소개";
+$nav_1_3 = "걸어온 길";
+$nav_1_4 = "오시는 길";
+$nav_1_4_1 = "본사 & 공장";
+$nav_1_4_2 = "쇼룸";
+
+$nav_2 = "중문 알아보기";
+$nav_2_1 = "명성아이지 중문의 특별함";
+
+$nav_2_2 = "다양한 중문 소개";
+$nav_2_2_1 = "연동도어";
+$nav_2_2_2 = "행거도어";
+$nav_2_2_3 = "자동도어";
+$nav_2_2_4 = "포켓도어";
+$nav_2_2_5 = "ㄱ자도어";
+$nav_2_2_6 = "스윙도어";
+
+$nav_2_3 = "최고의 유리 선택법";
+
+$nav_2_4 = "중문의 구성";
+$nav_2_4_1 = "3 Door";
+$nav_2_4_2 = "Swing Door";
+$nav_2_4_3 = "1 Door";
+$nav_2_4_4 = "3 Auto Door";
+
+$nav_3 = "제품소개";
+$nav_3_1 = "우리집에 맞는 중문찾기";
+$nav_3_2 = "프리미엄도어";
+$nav_3_3 = "초슬림 핵심도어";
+$nav_3_4 = "ㄱ자 파티션도어";
+$nav_3_5 = "스타일도어";
+$nav_3_6 = "원슬라이딩";
+$nav_3_7 = "포켓도어";
+$nav_3_8 = "유리종류";
+
+$nav_4 = "중문 솔루션";
+$nav_4_1 = "우리집에 맞는 중문찾기";
+$nav_4_1_1 = "Modern";
+$nav_4_1_1_kr = "모던";
+$nav_4_1_2 = "Classic";
+$nav_4_1_2_kr = "클래식";
+$nav_4_1_3 = "Stylish";
+$nav_4_1_3_kr = "스타일리쉬";
+$nav_4_1_1_1 = "어떤 공간의 분위기를 원하는가?";
+
+$nav_4_2 = "최고의 중문 선택법";
+
+$nav_5 = "홍보센터";
+$nav_5_1 = "뉴스";
+$nav_5_1_1 = "뉴스";
+$nav_5_1_2 = "이벤트";
+$nav_5_1_3 = "뽐내기";
+
+$nav_5_2 = "이벤트";
+$nav_5_2_1 = "뉴스";
+$nav_5_2_2 = "이벤트";
+$nav_5_2_3 = "뽐내기";
+
+$nav_5_3 = "뽐내기";
+$nav_5_3_1 = "뉴스";
+$nav_5_3_2 = "이벤트";
+$nav_5_3_3 = "뽐내기";
+
+$nav_6 = "고객지원";
+$nav_6_1 = "공지사항";
+$nav_6_1_1 = "공지사항";
+$nav_6_1_2 = "Q&A";
+$nav_6_1_3 = "FAQ";
+$nav_6_1_4 = "A/S신청";
+$nav_6_1_5 = "사업자 회원신청";
+
+$nav_6_2 = "Q&A";
+$nav_6_2 = "Q&A";
+$nav_6_2_1 = "공지사항";
+$nav_6_2_2 = "Q&A";
+$nav_6_2_3 = "FAQ";
+$nav_6_2_4 = "A/S신청";
+$nav_6_2_5 = "사업자 회원신청";
+
+
+$nav_6_3 = "FAQ";
+$nav_6_3 = "FAQ";
+$nav_6_3_1 = "공지사항";
+$nav_6_3_2 = "Q&A";
+$nav_6_3_3 = "FAQ";
+$nav_6_3_4 = "A/S신청";
+$nav_6_3_5 = "사업자 회원신청";
+
+$nav_6_4 = "A/S신청";
+$nav_6_4 = "A/S신청";
+$nav_6_4_1 = "공지사항";
+$nav_6_4_2 = "Q&A";
+$nav_6_4_3 = "FAQ";
+$nav_6_4_4 = "A/S신청";
+$nav_6_4_5 = "사업자 회원신청";
+
+$nav_6_5 = "사업자 회원신청";
+$nav_6_5 = "사업자 회원신청";
+$nav_6_5_1 = "공지사항";
+$nav_6_5_2 = "Q&A";
+$nav_6_5_3 = "FAQ";
+$nav_6_5_4 = "A/S신청";
+$nav_6_5_5 = "사업자 회원신청";
+
+$nav_policy = "홈페이지 정책";
+$nav_policy_1 = "개인정보처리방침";
+$nav_policy_2 = "이용약관";
+$nav_policy_3 = "이메일무단수집거부";
+
+$nav_login = "로그인";
+$nav_find = "아이디/비밀번호 찾기";
+$nav_join = "회원가입";
+$nav_login = "로그인";
+
+$nav_mypage = "마이페이지";
+
+// 네비게이션 하이퍼링크
+$nav_1_href = "../brand/ourbrand__solution.html";
+$nav_1_1_href = "../brand/ourbrand__solution.html#brnad";
+$nav_1_1_1_href = "../brand/ourbrand__solution.html#solution";
+$nav_1_1_2_href = "../brand/ourbrand__technic.html#technic";
+$nav_1_1_3_href = "../brand/ourbrand__service.html#service";
+
+$nav_1_2_href = "../brand/ci.html#ci";
+
+$nav_1_3_href = "../brand/history.html#history";
+
+$nav_1_4_href = "../brand/contact-us__head.html";
+$nav_1_4_1_href = "../brand/contact-us__head.html#head";
+$nav_1_4_2_href = "../brand/contact-us__showroom.html#showroom";
+/* $nav_1_4_2_href = "https://map.naver.com/v5/entry/place/13183404?c=14125622.2445745,4465002.5572969,13,0,0,0,dh&placePath=%2Fbooking%3FbookingRedirectUrl=https:%2F%2Fm.booking.naver.com%2Fbooking%2F6%2Fbizes%2F278975%26entry=plt"; */
+
+$nav_2_href = "../jungmun1/unique.html";
+$nav_2_1_href = "../jungmun1/unique.html#unique";
+
+$nav_2_2_href = "../jungmun1/variety__type.html#variety";
+$nav_2_2_1_href = "../jungmun1/variety__type.html#type1";
+$nav_2_2_2_href = "../jungmun1/variety__type.html#type2";
+$nav_2_2_3_href = "../jungmun1/variety__type.html#type3";
+$nav_2_2_4_href = "../jungmun1/variety__type.html#type4";
+$nav_2_2_5_href = "../jungmun1/variety__type.html#type5";
+$nav_2_2_6_href = "../jungmun1/variety__type.html#type6";
+
+$nav_2_3_href = "../jungmun1/best-glass.html#glass";
+
+$nav_2_4_href = "../jungmun1/kind__door.html#door";
+$nav_2_4_1_href = "../jungmun1/kind__door.html#kind1";
+$nav_2_4_2_href = "../jungmun1/kind__door.html#kind2";
+$nav_2_4_3_href = "../jungmun1/kind__door.html#kind3";
+$nav_2_4_4_href = "../jungmun1/kind__door.html#kind4";
+
+$nav_3_href = "../product/custom.html";
+$nav_3_1_href = "../product/custom.html#custom";
+$nav_3_2_href = "../product/customview.html";
+
+$nav_4_href = "../jungmun2/myhome__type.html";
+$nav_4_1_href = "../jungmun2/myhome__type.html#modern";
+$nav_4_1_1_href = "../jungmun2/myhome__type.html#modern";
+$nav_4_1_2_href = "../jungmun2/myhome__type.html#classic";
+$nav_4_1_3_href = "../jungmun2/myhome__type.html#stylish";
+
+$nav_4_2_href = "../jungmun2/best-door.html#best";
+
+$nav_5_href = "../promotion/news.html";
+$nav_5_1_href = "../promotion/news.html#news";
+
+$nav_5_2_href = "../promotion/event.html#event";
+$nav_5_3_href = "../promotion/showoff.html#showoff";
+
+$nav_6_href = "../customer/notice.html";
+$nav_6_1_href = "../customer/notice.html#notice";
+$nav_6_2_href = "../customer/qna.html#qna";
+$nav_6_3_href = "../customer/faq.html#faq";
+$nav_6_4_href = " ../customer/as.html#as";
+$nav_6_5_href = "http://msig0352.cafe24.com/board/free/list.html?board_no=5";
+
+$nav_policy_href = "../policy/privacy.html";
+$nav_policy_1_href = "../policy/privacy.html";
+$nav_policy_2_href = "../policy/term.html";
+$nav_policy_3_href = "../policy/email-security.html";
+
+// 서브비주얼
+$sv_1_name = "";
+$sv_2_name = "";
+$sv_3_name = "";
+$sv_4_name = "";
+$sv_5_name = "";
+
+//gnb 메뉴
+$nav_catalog = "E-카다로그";
+$nav_catalog_1 = "E-카다로그";
+$nav_catalog_1_href = "../catalog/catalog-board.html";
+
+?>
